a64dabad8e498f5dfe53324afcbac094feba39fb
19110254@student.hcmute.edu.vn <19110254@student.hcmute.edu.vn>
2023-09-05 21:04:54 +0700

    set up comment

--- src/main/java/com/example/officepcstore/models/enity/Comment.java
+++ src/main/java/com/example/officepcstore/models/enity/Comment.java
@@ -1,11 +1,41 @@
 package com.example.officepcstore.models.enity;
 
+import com.example.officepcstore.models.enity.product.Product;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.time.LocalDateTime;
 
 @Document(collection = "comments")
 @Data
+@NoArgsConstructor
 public class Comment {
+    @Id
+    private String id;
+    private String review;
+    private double rate;
+    @DocumentReference(lazy = true)
+    @JsonIgnore
+    @Indexed
+    private User user;
+    @DocumentReference(lazy = true)
+    @JsonIgnore
+    @Indexed
+    private Product product;
+    private String enable;
+    @CreatedDate
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    LocalDateTime createdDate;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    @LastModifiedDate
+    LocalDateTime lastModifiedDate;
 }
